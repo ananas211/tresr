24d17ac7097f1fdd155498276cd4a67150e279f9
ananas211 <146007989+ananas211@users.noreply.github.com>
2023-10-13 08:06:02 -0700

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,25 @@
+WilliamAfton=12
+MoM=35
+DaD=39
+cAt=3
+
+#print current ages 
+
+print(f"WilliamAfton:{WilliamAfton}")
+print(f"MoM:{MoM}")
+print(f"DaD:{DaD}")
+print(f"cAt:{cAt}")
+
+#calculate ages after 15 years
+
+William_Afton_after_15_years=WilliamAfton+15
+MoM_age_after_15_years=MoM+15
+Dad_age_after_15_years=DaD+15
+cAt_age_after_15_years=cAt+15
+
+#print after 15 years
+
+print(f"William Afton after 15 years:{William_Afton_after_15_years}")
+print(f"MoM age after 15 years:{MoM_age_after_15_years}")
+print(f"DaD age after 15 years:{Dad_age_after_15_years}")
+print(f"cAt age after 15 years:{cAt_age_after_15_years}")
